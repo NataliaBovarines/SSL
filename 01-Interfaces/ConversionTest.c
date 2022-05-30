72a81bfbb8da5b67d13edc1804b8f37cc6954b66
NataliaBovarines <natalia.bovarines@gmail.com>
2022-05-29 23:39:08 -0300

    renombro parametros de funcion bool

--- 01-Interfaces/ConversionTest.c
+++ 01-Interfaces/ConversionTest.c
@@ -13,21 +13,21 @@ bool casi_iguales(double , double);
 
 int main(){
     
-    //0°C es casi igual a 32° F
+    //0°C es casi igual a 32°F
     assert(casi_iguales(Farenheit(0),32.0));
 
     //60°F es casi igual a 15.5°C
     assert(casi_iguales(Celsius(60.0),15.5));
 
-    //20°F no es casi igual a -8° C
+    //20°F no es casi igual a -8°C
     assert(!casi_iguales(Celsius(20),-8));
 
 }
 
 
-bool casi_iguales(double izquierda, double derecha)
+bool casi_iguales(double valor1, double valor2)
 {
     double epsilon =  0.1;
 
-    return fabs(izquierda - derecha) <= epsilon;
+    return fabs(valor1 - valor2) <= epsilon;
 }
